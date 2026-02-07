f35b1a9dd69f63148da720b0dd6cad0788248364
Ashvij <ashvijhosdurg@gmail.com>
2026-02-07 17:34:33 -0500

    style and attempt with json

--- character_info.js
+++ character_info.js
@@ -1,5 +1,16 @@
+
+
+
+
 function updateCharacterSheet(data) {
     document.getElementById("character-name").textContent = data.name;
+    document.getElementById("class-name").textContent = data.class;
+    document.getElementById("race-name").textContent = data.race;
+    document.getElementById("backstory").textContent = data.backstory;
+    document.getElementById("class-description").textContent = "a dude";
     document.getElementById("result").textContent = data.character_sheet;
-
 }
+
+
+
+
